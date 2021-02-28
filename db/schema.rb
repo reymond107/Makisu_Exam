800667731c0e2ceda5ce30c09078badcca3b0212
reymond107 <reymondcastillon@gmailcom>
2021-02-28 21:24:24 +0800

    successfully changed the database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_23_011325) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "searches", force: :cascade do |t|
     t.string "website"
     t.string "status"
